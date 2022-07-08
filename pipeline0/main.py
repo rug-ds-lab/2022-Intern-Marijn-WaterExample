248986ff84bb2ddc5cef836744761ef4aaeef1ed
marijn <marijnschokker@icloud.com>
2022-07-08 16:00:38 +0200

    Basic data streaming with Kafka and data saving set up with InfluxDB

--- pipeline0/main.py
+++ pipeline0/main.py
@@ -1,4 +1,7 @@
 from kafka import KafkaConsumer
+from influxdb_client import InfluxDBClient, Point
+from influxdb_client.client.write_api import SYNCHRONOUS
+import json
 
 KAFKA_SERVER = 'kafka:9092'
 
@@ -7,8 +10,15 @@ def run():
     consumer = KafkaConsumer('dma-data', bootstrap_servers=KAFKA_SERVER)
     print('pipeline0 started')
 
+    client = InfluxDBClient(url='http://influxdb:8086', username='admin', password='bitnami123', org='primary')
+    write_api = client.write_api(write_options=SYNCHRONOUS)
+
     for msg in consumer:
-        print(msg)
+        measurement = json.loads(msg.value.decode('utf-8'))
+        print(measurement)
+
+        p = Point('measurement').tag('node', '2').field('pressure', measurement['2'])
+        write_api.write(bucket='primary', record=p)
 
 
 if __name__ == '__main__':
--- producer/main.py
+++ producer/main.py
@@ -1,15 +1,38 @@
 from kafka import KafkaProducer
+import pandas as pd
+import glob
+import re
+import os
+from time import sleep
 
 KAFKA_SERVER = 'kafka:9092'
+
+
+# noinspection DuplicatedCode
+def read_pressure_as_matrix(folder_path):
+    pressures = pd.DataFrame()
+    for link_path in glob.glob(folder_path + '/Node_*.csv'):
+        node_pressure = pd.read_csv(link_path)['Value']
+        node_n = int(re.sub('\D', '', os.path.basename(link_path)))
+        pressures[node_n] = node_pressure
+    return pressures.reindex(sorted(pressures.columns), axis=1)
 
 
 def run():
     producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER)
     print('Producer started')
+    data_path = os.environ.get('DATA_PATH')
+    scenario = 'Scenario-13/'
+    values = 'Pressures'
+    print(data_path + scenario)
+    pressures = read_pressure_as_matrix(folder_path=data_path + scenario + values)
+    print(pressures)
 
-    for _ in range(0, 20):
-        producer.send('dma-data', b'message')
+    for i in pressures.index:
+        current_pressure = pressures.iloc[i].to_json()
+        producer.send('dma-data', current_pressure.encode('utf-8'))
         producer.flush()
+        sleep(3)
 
 
 if __name__ == '__main__':
