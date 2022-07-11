b720a7056b539ea8dd09a1f80b1b0a1102b4e341
marijn <marijnschokker@icloud.com>
2022-07-11 21:23:47 +0200

    Flow data storing and visualization

--- producer/main.py
+++ producer/main.py
@@ -13,10 +13,21 @@ from configparser import ConfigParser
 KAFKA_SERVER = 'kafka:9092'
 
 
-# noinspection DuplicatedCode
-def read_pressure_as_matrix(folder_path):
+def read_data_as_matrix(folder_path, network_property):
     pressures = pd.DataFrame()
-    for link_path in glob.glob(folder_path + '/Node_*.csv'):
+
+    if network_property == 'Pressures':
+        file_name = 'Node_*.csv'
+    elif network_property == 'Flows':
+        file_name = 'Link_*.csv'
+    else:
+        raise Exception('Incorrect property')
+
+    data_path = os.path.join(folder_path, network_property, file_name)
+
+    print(data_path)
+
+    for link_path in glob.glob(data_path):
         node_pressure = pd.read_csv(link_path)['Value']
         node_n = int(re.sub('\D', '', os.path.basename(link_path)))
         pressures[node_n] = node_pressure
@@ -30,12 +41,10 @@ def run():
     config = ConfigParser()
     config.read('config.ini')
     scenario_path = config['DEFAULT']['scenario_path']
-    network_property = config['DEFAULT']['network_property']
     start_time = int(config['DEFAULT']['start_time'])
 
-    data_path = os.path.join(scenario_path, network_property)
-
-    pressures = read_pressure_as_matrix(folder_path=data_path)
+    pressures = read_data_as_matrix(folder_path=scenario_path, network_property='Pressures')
+    flows = read_data_as_matrix(folder_path=scenario_path, network_property='Flows')
     print(pressures)
 
     timestamp_path = os.path.join(scenario_path, 'Timestamps.csv')
@@ -44,17 +53,18 @@ def run():
 
     # Scenario 13: Leak Start is at 6587
     pressures = pressures.iloc[start_time:, :].reset_index(drop=True)
+    flows = flows.iloc[start_time:, :].reset_index(drop=True)
     timestamps = timestamps.iloc[start_time:, :].reset_index(drop=True)
 
     client = InfluxDBClient(url='http://influxdb:8086', username='admin', password='bitnami123', org='primary')
     write_api = client.write_api(write_options=SYNCHRONOUS)
 
-    for i in pressures.index:
-        current_pressure = pressures.iloc[i]
-        print(timestamps.iloc[i])
+    for p, f in zip(pressures.index, flows.index):
+        current_pressure = pressures.iloc[p]
+        print(timestamps.iloc[p])
 
         current_time = datetime\
-            .fromisoformat(timestamps.iloc[i]['Timestamp'])\
+            .fromisoformat(timestamps.iloc[p]['Timestamp'])\
             .timetuple()\
 
         unix_time = int(mktime(current_time))
@@ -66,8 +76,18 @@ def run():
                 .time(unix_time, write_precision='s')
             write_api.write(bucket='primary', record=p)
 
+        current_flow = flows.iloc[f]
+
+        for link in current_flow.index:
+            p = Point('measurement') \
+                .tag('link', link) \
+                .field('flow', current_flow[link]) \
+                .time(unix_time, write_precision='s')
+            write_api.write(bucket='primary', record=p)
+
         current_pressure['time'] = unix_time
         current_pressure_json = current_pressure.to_json()
+
         producer.send('dma-epynet_data', current_pressure_json.encode('utf-8'))
         producer.flush()
         sleep(0.5)
